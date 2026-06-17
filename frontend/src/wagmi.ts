5a55a2078fef1f599ddc0314d7d13ccda11cd766
0xavp <a.pavlyunin@tutanota.com>
2026-06-17 23:09:23 +0200

    chore: add wagmi-cli

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -1,13 +1,14 @@
 import { createConfig, http } from 'wagmi';
-import { baseSepolia, sepolia } from 'wagmi/chains';
+import { baseSepolia, sepolia, hardhat } from 'wagmi/chains';
 import { injected } from 'wagmi/connectors';
 
 export const config = createConfig({
-    chains: [baseSepolia, sepolia],
+    chains: [hardhat, baseSepolia, sepolia],
     connectors: [
         injected(),
     ],
     transports: {
+        [hardhat.id]: http('http://127.0.0.1:8545'),
         [baseSepolia.id]: http(),
         [sepolia.id]: http(),
     },
---
+++ frontend/wagmi.config.ts
@@ -0,0 +1,13 @@
+// frontend/wagmi.config.ts
+import { defineConfig } from '@wagmi/cli'
+import { foundry, react } from '@wagmi/cli/plugins'
+
+export default defineConfig({
+    out: 'src/generated.ts',
+    plugins: [
+        foundry({
+            project: '../contracts',
+        }),
+        react(),
+    ],
+})
